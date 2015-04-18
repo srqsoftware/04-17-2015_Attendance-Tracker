5062be4176f78db4b7b74ac72f481d77bc2347e2
Frank Cash <cfcash@mtu.edu>
2015-04-17 22:28:44 -0400

    Edits Meetup API call

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 var request=require("request");
 var express=require("express");
+var config=require("./config/config");
 var app = express(); // sets up the server
-var baseUrl = "api.meetup.com";
-var eventGet = "/2/event/"; // add ID after
+
+var baseUrl = "http://api.meetup.com/";
+var eventGet = "/events/:id/attendance "; // add ID after
+
 app.set('views', __dirname + '/views'); // sets dir
 
 app.set('view engine', 'jade');
@@ -10,15 +13,22 @@ app.set('view engine', 'jade');
 
 app.use(express.static(__dirname + '/public'));
 
-app.get('/', function (req, res) {
-  request('', function(error, response, html){
+app.get('/users', function (req, res) {
+  var eventId = "221682391/";
+  var groupName = "SarasotaSoftwareEngineers/";
+  request( baseUrl + groupName + "events/" + eventId + "attendance" + "?key=" + config.meetup, function(error, response, body){
     if(!error && response.statusCode === 200){
-      return metadataArray;
+      var resp = JSON.parse(body);
+      console.log("info", resp);
+      res.sendStatus(200);
+    }else{
+      console.log(error);
+      console.log(config);
+      res.sendStatus(400);
     }
   });
 
 
-  res.send(200);
 
 });
 
